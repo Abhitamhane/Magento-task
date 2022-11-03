735e1827f294ad40559c61ca2f89cf6e89d2c6f7
Abhishek Tamhane <Abhishek.Tamhane@ezest.local>
2022-11-03 21:08:03 +0530

    first commit

---
+++ app/code/Magento/Employee/HelloWorld/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrer::register(
+\Magento\Framework\Component\ComponentRegistrer::MODULE,
+'Employee_HelloWorld',
+__DIR__
+);
---
+++ app/etc/env.php
@@ -0,0 +1,85 @@
+<?php
+return [
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => 'eb4398bcba30598501b0f28c8c16c5e0'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento2',
+                'username' => 'magento2',
+                'password' => 'Abhi@1289',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'b6e_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'b6e_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => null
+        ]
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'abhi.magento2.com'
+    ],
+    'install' => [
+        'date' => 'Wed, 02 Nov 2022 17:33:21 +0000'
+    ]
+];
